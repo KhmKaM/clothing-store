00531d27df1fa1ae503027cc6e9f803c68ab3d9a
Kamila Khairullina <khkamila331@gmail.com>
2023-02-15 00:28:17 +0300

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        items: [
+          {
+            id:1,
+            title: 'test product',
+            price: 2300,
+            desc: 'lorem ipsum set',
+            img: '1.jpg',
+            category: 'cloth'
+          },
+          {
+            id:2,
+            title: 'test product',
+            price: 400,
+            desc: 'lorem ipsum set',
+            img: '2.jpg',
+            category: 'cloth'
+          },
+          {
+            id:3,
+            title: 'test product',
+            price: 500,
+            desc: 'lorem ipsum set',
+            img: '3.jpg',
+            category: 'cloth'
+          },
+          {
+            id:4,
+            title: 'test product',
+            price: 1300,
+            desc: 'lorem ipsum set',
+            img: '4.jpg',
+            category: 'cloth'
+          },
+          {
+            id:5,
+            title: 'test product',
+            price: 1500,
+            desc: 'lorem ipsum set',
+            img: '5.jpg',
+            category: 'cloth'
+          }
+        ]
+      }
+    }
+    render() {
+      return (
+        <div className="wrapper">
+          <Header/>
+          <Items items={this.state.items}/>
+          <Footer/>
+        </div>
+      );
+  }
 }
 
 export default App;
